a421f248bc0821362535433c24d36306256cc2d5
ConradMar3 <conradmare890317@gmail.com>
2022-12-01 13:20:30 +0200

    2022/12/01 (pm)
    SpringBoot - Chapter 02.
    Developing Web Applications.

---
+++ Workspaces/taco-cloud-ch02/src/test/java/tacos/TacoCloudCh02ApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudCh02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
